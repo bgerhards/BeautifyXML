d37aadc3ce58574f3b4bb8d3f4a60f04316c3db7
Brian Gerhards <briangerhards@gmail.com>
2016-06-28 15:47:19 -0500

    Removed gutter

--- js/xmlFormatter.js
+++ js/xmlFormatter.js
@@ -10,10 +10,12 @@ $(document).ready(function() {
     editor.setOptions({
       fontSize: "12pt"
     });
-    // editor.setValue("<?xml version=\"1.0\"?><catalog><book id=\"bk101\"><author>Gambardella, Matthew</author><title>XML Developer's Guide</title><genre>Computer</genre><price>44.95</price><publish_date>2000-10-01</publish_date><description>An in-depth look at creating applications with XML.</description></book><book id=\"bk102\"><author>Ralls, Kim</author><title>Midnight Rain</title><genre>Fantasy</genre><price>5.95</price><publish_date>2000-12-16</publish_date><description>A former architect battles corporate zombies, an evil sorceress, and her own childhood to become queen of the world.</description></book></catalog>")
-    // beautify();
-    // $('div').blur();
-    // alert(editor.findAll('book'));
+
+
+    // Hiding the gutter for testing a new layout to the site
+    editor.renderer.setShowGutter(false);
+
+
     sampleData[1] = "<?xml version=\"1.0\"?><catalog><book id=\"bk101\"><author>Gambardella, Matthew</author><title>XML Developer\'s Guide</title><genre>Computer</genre><price>44.95</price><publish_date>2000-10-01</publish_date><description>An in-depth look at creating applicationswith XML.</description></book><book id=\"bk102\"><author>Ralls, Kim</author><title>Midnight Rain</title><genre>Fantasy</genre><price>5.95</price><publish_date>2000-12-16</publish_date><description>A former architect battles corporate zombies,an evil sorceress, and her own childhood to become queenof the world.</description></book><book id=\"bk103\"><author>Corets, Eva</author><title>Maeve Ascendant</title><genre>Fantasy</genre><price>5.95</price><publish_date>2000-11-17</publish_date><description>After the collapse of a nanotechnologysociety in England, the young survivors lay thefoundation for a new society.</description></book></catalog>";
 });
 
